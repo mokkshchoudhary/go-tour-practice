91d3428f9ab60aac6d3e8ea7a5a703879e309063
Moksh Choudhary <mokkshchoudhary@gmail.com>
2025-11-13 08:20:22 +0000

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+const pi = 3.14
+
+func main(){
+	const world = "Rahat"
+	fmt.Println("Hello", world)
+	fmt.Println("happy", pi, "day")
+
+	const Truth = true
+	fmt.Println("should we follow rules ? ", Truth)
+}
